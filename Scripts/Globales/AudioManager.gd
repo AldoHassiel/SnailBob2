7d54d8aa735237e9ba01f46f8960cd81188cb5fa
Aldo Hassiel <110874704+AldoHassiel@users.noreply.github.com>
2025-09-26 16:47:40 -0700

    Add sound toggle button and global audio settings
    Introduces BotonSonido for toggling background music, with supporting script and scene. Adds DatosGlobales for centralized audio configuration and updates AudioManager to track music state and respond to toggle actions. Registers DatosGlobales as an autoload singleton in project settings.

--- Scripts/Globales/AudioManager.gd
+++ Scripts/Globales/AudioManager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var hay_musica_reproduciendose : bool = DatosGlobales.REPRODUCIR_MUSICA_POR_DEFECTO
+
 var reproductor_musica: AudioStreamPlayer
 var reproductor_sonido : AudioStreamPlayer
 
@@ -14,6 +16,9 @@ func _ready() -> void:
 	reproductor_sonido = AudioStreamPlayer.new()
 	reproductor_sonido.bus = "Sonido"
 	add_child(reproductor_sonido)
+	
+	if DatosGlobales.REPRODUCIR_MUSICA_POR_DEFECTO:
+		reproducir_musica(DatosGlobales.MUSICA_POR_DEFECTO)
 
 # Metodos para la musica
 func reproducir_musica(pista : AudioStream):
@@ -23,9 +28,14 @@ func reproducir_musica(pista : AudioStream):
 	reproductor_musica.stream = pista
 	reproductor_musica.volume_db = linear_to_db(volumen_musica)
 	reproductor_musica.play()
+	
+	hay_musica_reproduciendose = true
 
 func detener_musica():
 	reproductor_musica.stop()
+	reproductor_musica.stream = null
+	
+	hay_musica_reproduciendose = false
 
 func establecer_volumen_musica(valor : float):
 	volumen_musica = clamp(valor, 0.0, 1.0)
@@ -39,6 +49,7 @@ func reproducir_sonido(efecto : AudioStream):
 
 func detener_sonido():
 	reproductor_sonido.stop()
+	reproductor_sonido.stream = null
 
 func establecer_volumen_sonido(valor: float):
 	volumen_sonido = clamp(valor, 0.0, 1.1)
